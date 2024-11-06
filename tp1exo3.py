1b35fd1720740f096616e17ac9291837beb9d767
chlo070 <chloesondag.pro@gmail.com>
2024-11-06 11:44:54 +0100

    essai

--- tp1exo3.py
+++ tp1exo3.py
@@ -3,3 +3,4 @@ heure=int (input("Donner l'heure d'aujourd'hui : "))
 minute=int (input("Donner le nombre de minutes dans cette heure : "))
 var = (((jour-1) * 24)*60) + (heure*60) + minute
 print ("Le nombre de minutes écoulées depuis le début du mois est de : " , var)
+fghcfg
