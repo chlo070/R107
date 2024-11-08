5c3b8df5471fd1df08b00401637fc1c184edb234
chlo0700 <chloeondag.pro@gmail.Com>
2024-11-08 16:12:06 +0100

    test

--- TP2/Exo5_instructions.py
+++ TP2/Exo5_instructions.py
@@ -1,10 +1,10 @@
-n=int (input("Rentrez un nombre :"))
+n=int (input("Entrez un nombre :"))
 if n<0 :
     if n%2==0 :
         print(n, " est un nombre négatif et pair.")
     else :
         print(n, " est un nombre négatif et impair.")
-if n>0 :
+elif n>0 :
     if n%2==0 :
         print(n, " est un nombre positif et pair.")
     else :
