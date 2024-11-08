eac7b53ae393820c8fddf42e47469455bd3f0cad
chlo0700 <chloeondag.pro@gmail.Com>
2024-11-08 15:46:28 +0100

    test

--- TP2/Exo4_Fondue.py
+++ TP2/Exo4_Fondue.py
@@ -1,6 +1,15 @@
-base=4
-fromage=800*base
-eau=2*base
-ail=2*base
-pain=400*base
+BASE=4
+fromage=800
+eau=2
+ail=2
+pain=400
 nbConvives=int (input("Pour combien de convive veut-on préparer la recette ? "))
+varFr = fromage * (nbConvives / BASE)
+varEau = eau * (nbConvives / BASE)
+varAil = ail * (nbConvives / BASE)
+varP = pain * (nbConvives / BASE)
+print("Pour faire une fondue fribourgeoise pour ", nbConvives, " personnes, il vous faut :")
+print("- ", varFr, " gr de fromage")
+print("- ", varEau, " dl d'eau")
+print("- ", varAil, " gousse(s) d'ail")
+print("- ", varP, "gr de pain")
