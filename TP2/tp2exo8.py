69e73e3f5aa94520722b4ae75b7a946a39a52edd
chlo0700 <chloeondag.pro@gmail.Com>
2024-11-08 16:40:45 +0100

    test

--- TP2/tp2exo8.py
+++ TP2/tp2exo8.py
@@ -1,6 +1,6 @@
 R=float (input("Entrez un nombre décimal :"))
 x=R
-if (2<=x<3) or :
+if (2<=x<3) or (0<x<=1) or (-10<=x<=-2):
     print(x, " appartient à I.")
 else :
     print(x, " n'appartient pas à I.")
