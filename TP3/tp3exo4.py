8784f322996cb178b5f037a9909f2a2d447f3406
chlo070 <chloesondag.pro@gmail.com>
2024-11-22 17:32:09 +0100

    test

--- TP3/tp3exo4.py
+++ TP3/tp3exo4.py
@@ -1,15 +1,16 @@
 n=int(input("Entrer un nombre entier :"))
-b=input("Quelle boucle utiliser ? (for/while)")
+b=input("Quelle boucle utiliser ? (for/while) : ")
 f=1
+while b!="for" and b!="while":
+    b = input("Erreur de frappe, recommencez (for/while) : ")
+
 if b=="for" :
-    for i in range (1, n+1) :
-        f *= i
-        print(f)
-elif b=="while" :
-    j=1
-    while j<=n:
-        f *= j
-        print(f)
-        j += 1
+        for i in range (1, n+1) :
+            f *= i
+            print(f)
 else :
-    b=input("Erreur de frappe, recommencez : ")
+        j=1
+        while j<=n:
+            f *= j
+            print(f)
+            j += 1
