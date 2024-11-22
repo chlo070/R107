ae1f159cbba4878542422d5b4cb427bb75a9e49c
chlo070 <chloesondag.pro@gmail.com>
2024-11-22 23:25:06 +0100

    Update tp3exo5.py
    solo

--- TP3/tp3exo5.py
+++ TP3/tp3exo5.py
@@ -1,1 +1,20 @@
-while debut == fin or debut > 25 or fin < 0
+print("De quelle heure à quelle heure se fait la location?")
+debut=int(input("Heure de début (un entier)")
+fin=int(input("Heure de fin (un entier)")
+prix=int
+tarif1=int
+tarif2=int
+
+if debut
+        
+while debut == fin or debut > 25 or fin < 0 or debut > fin :
+  print("Les heures doivent être comprises entre 0 et 24" end"")
+  print("Attention ! L'heure de fin est identique à l'heure de début" end "")
+  print("Attention ! Le début de la location est après la fin..." end "")
+  debut=int(input("Heure de début (un entier)")
+  fin=int(input("Heure de fin (un entier)")
+  
+
+print("Vous avez loué votre vélo pendant")
+print(
+          
