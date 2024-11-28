63e9fada55b7c86ae6d8ea160df1a5bbf02eec63
chlo070 <chloesondag.pro@gmail.com>
2024-11-28 11:14:24 +0100

    'lo

--- TP4/tp4exo1.py
+++ TP4/tp4exo1.py
@@ -1,21 +1,28 @@
-import round(x,n)
+
+n=float(input("Vous cherchez la table de multiplication de quel nombre ?:"))
+table=[]
+i=0
+for i in range (10) :
+    res= round(n * i,1)
+    table.append(res)
+
+print(table)
+for i in range(10):
+    print(f"{n} * {i} = {table[i]}")
 
 
 
+#round(x,n)
+#exemple de sortie :Vous cherchez la table de multiplication de quel nombre ?
+#1.2 * 0 = 0.0
+#1.2 * 1 = 1.2
+#1.2 * 2 = 2.4
+#1.2 * 3 = 3.6
+#1.2 * 4 = 4.8
+#1.2 * 5 = 6.0
+#1.2 * 6 = 7.2
+#1.2 * 7 = 8.4
+#1.2 * 8 = 9.6
+#1.2 * 9 = 10.8
 
-
-
-#exemple :
-Vous cherchez la table de multiplication de quel nombre ?
-1.2 * 0 = 0.0
-1.2 * 1 = 1.2
-1.2 * 2 = 2.4
-1.2 * 3 = 3.6
-1.2 * 4 = 4.8
-1.2 * 5 = 6.0
-1.2 * 6 = 7.2
-1.2 * 7 = 8.4
-1.2 * 8 = 9.6
-1.2 * 9 = 10.8
-#
 
