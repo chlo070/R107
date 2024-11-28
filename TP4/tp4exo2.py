aace89ac1827df0ddd3527c5ef0e4b095bc008f0
chlo070 <chloesondag.pro@gmail.com>
2024-11-28 11:36:15 +0100

    'lo

--- TP4/tp4exo2.py
+++ TP4/tp4exo2.py
@@ -3,19 +3,35 @@ nombreEtudiants = int(input("Donnez le nombre d'etudiants : "))
 moyenne = 0.0
 # déclaration d’une liste vide qui va contenir autant de notes que d'étudiants
 notes = []
+i=0
+somme=0
+for i in range (nombreEtudiants):
+    note = int(input(f"Donnez la note de l'etudiant {round(i,1)}: "))
+    notes.append(note)
+    somme += note
+
+moyenne=somme/nombreEtudiants
+
+print(f"Moyenne de classe : {moyenne}")
+
+
+
+
+
+
 
 #exemple:
-Donnez le nombre d'etudiants : 4
-Note etudiant 0 : 4.0
-Note etudiant 1 : 4.0
-Note etudiant 2 : 5.0
-Note etudiant 3 : 6.0
-Moyenne de classe : 4.75
-Numéro de l’Etudiant | note | ecart a la moyenne
-0 | 4.0 | -0.75
-1 | 4.0 | -0.75
-2 | 5.0 | 0.25
-3 | 6.0 | 1.25
-#
+#Donnez le nombre d'etudiants : 4
+#Note etudiant 0 : 4.0
+#Note etudiant 1 : 4.0
+#Note etudiant 2 : 5.0
+#Note etudiant 3 : 6.0
+#Moyenne de classe : 4.75
+#Numéro de l’Etudiant | note | ecart a la moyenne
+#0 | 4.0 | -0.75
+#1 | 4.0 | -0.75
+#2 | 5.0 | 0.25
+#3 | 6.0 | 1.25
+
 
 
