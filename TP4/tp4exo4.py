ad4f267c67a6e741e385990bd027d21ecdb3d2cd
e2400096 <chloesondag.pro@gmail.com>
2024-12-11 11:59:59 +0100

    yo

--- TP4/tp4exo4.py
+++ TP4/tp4exo4.py
@@ -5,26 +5,31 @@ L1 = [2, 7, 5, 6, 7, 1, 6, 2, 1, 7, 6]
 ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** * /
 """
 #Partie1
+print(L1)
 n = len(L1)
 occurence = 0
 maxOcc=0
 maxIndice = 0
+nIndice = 0
 for i in range (n) :
     for j in range (i+1, n) :
         if L1[i] == L1[j] :
             occurence += 1
             if occurence > maxOcc :
                 maxOcc = occurence
-                MaxIndice = L1[i]
+                maxIndice = L1[i]
+                nIndice += 1
 
-L1.index(maxIndice)
+if nIndice!=1 :
+    maxIndice = L1[]
+#(L1.index(maxIndice))
 
-print(f"Le nombre le plus frequent dans la liste est le : , {maxIndice} ({occurence} x)")
+print(f"Le nombre le plus frequent dans la liste est le : {maxIndice} ({L1.index(maxIndice)}x)")
 
 
 #Partie2
-n = len(L1)
-count()
+#n = len(L1)
+#L1.count(L1[i])
 
 
 
---
+++ TP4/tp4exo5-VerificationDate.py
@@ -0,0 +1,22 @@
+#traiter jjmmaaaa comme une liste
+# position 0=j1, position 1=j2, 2=m1, 3=m2, 4=a1, 5=a2, 6=a3, 7=a4
+
+taille = 8
+
+
+#demander les valeurs de la position 0 à 2 exclu
+jour = int(date [0:2])
+mois = int(date [2:4])
+année = int(date [4:8])
+
+
+while 0 < année or année 9999 or 0 < mois or mois>12 or jour >1 or jour > 31 :
+    print("Date incorrecte, entrez une nouvelle date :")
+    #ou msg = "Date incorrecte"
+
+#Pour une année bisextile :
+if mois == 2 :
+    if ((année ))
+
+
+print (msg)
