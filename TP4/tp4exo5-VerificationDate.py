2f96bef2e656b7c4b8049f0cc672a99b9f7231b1
chlo070 <chloesondag.pro@gmail.com>
2024-12-11 16:46:11 +0100

    yo

--- TP4/tp4exo5-VerificationDate.py
+++ TP4/tp4exo5-VerificationDate.py
@@ -2,31 +2,39 @@
 #traiter jjmmaaaa comme une liste
 #position 0=j1, position 1=j2, 2=m1, 3=m2, 4=a1, 5=a2, 6=a3, 7=a4
 
-taille = 8
-date=[]
+
 msg = "date correcte"
 
-#demander les valeurs de la position 0 à 2 exclu
-jour = int(jjmmaaaa[0]*10 + jjmmaaaa[1])
-jour = int(input(date [0:2]))
-mois = int(input(date [2:4]))
-annee = int(input(date [4:8]))
+date=input("Quelle est la date?:")
+jour = int(date[0]*10 + date[1])
+mois = int(date[2]*10 + date[3])
+annee = int(date[4]*1000 + date[5]*100 + date[6]*10 + date[7])
 
-print (f{jour}/{mois}/{annee})
+print (f"{jour}/{mois}/{annee}")
 
+"""
 while 0 > annee or annee > 9999 or 0 > mois or mois>12 or jour < 1 or jour > 31 :
     print("Date incorrecte, entrez une nouvelle date :")
-    #ou msg = "Date incorrecte"
-    #puis print(msg)
-    jour = int(input("Quel est le jour?:"))
-    jour = int(input(date[0:2]))
-    mois = int(input(date[2:4]))
-    annee = int(input(date[4:8]))
+    jour = int(jjmmaaaa[0] * 10 + jjmmaaaa[1])
+    mois = int(jjmmaaaa[0] * 10 + jjmmaaaa[1])
+    annee = int(jjmmaaaa[0] * 1000 + jjmmaaaa[0] * 100 + jjmmaaaa[0] * 10 + jjmmaaaa[1])
 
 #Pour une année bisextile :
 if mois == 2 :
    if ((annee%4 == 0 and annee%100!=0) or annee%400 == 0) :
-       if (1 <= jour and jour <= 29)
+       if (1 <= jour and jour <= 29) :
+            msg = "date correcte"
+
+if mois == 2 :
+   if ((annee%4 == 0 and annee%100!=0) or annee%400 == 0) :
+       if jour > 29 :
+           msg = "date incorrecte"
+   else:
+       if jour > 28 :
+           msg = ("date incorrecte")
+elif mois not in m31 :
+    if jour > 30 :
+        msg = ("date incorrecte")
 
 
 #lilste des mois à 31jrs
@@ -36,7 +44,12 @@ m31=[1,3,5,7,8,10,12]
 
 
 #autre methode
+
+#demander les valeurs de la position 0 à 8 exclu
 date = input ("Entrez une date de la forme jjmmaaaa :")
 jour = int(date[0:2])
 mois = int(date[2:4])
 annee = int(date[4:8])
+
+print (f"{jour}/{mois}/{annee}")
+"""
