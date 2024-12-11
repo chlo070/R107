0d33c9a99a69145ae92b13858ca819ac77429302
chlo070 <chloesondag.pro@gmail.com>
2024-12-11 15:39:01 +0100

    yo

--- TP4/tp4exo5-VerificationDate.py
+++ TP4/tp4exo5-VerificationDate.py
@@ -2,23 +2,23 @@
 #traiter jjmmaaaa comme une liste
 #position 0=j1, position 1=j2, 2=m1, 3=m2, 4=a1, 5=a2, 6=a3, 7=a4
 
-Date=[]
 taille = 8
-
+date=[]
 
 #demander les valeurs de la position 0 à 2 exclu
+
 jour = int(date [0:2])
 mois = int(date [2:4])
-année = int(date [4:8])
+annee = int(date [4:8])
 
 
-while 0 < année or année 9999 or 0 < mois or mois>12 or jour >1 or jour > 31 :
+while 0 > annee or annee > 9999 or 0 > mois or mois>12 or jour < 1 or jour > 31 :
     print("Date incorrecte, entrez une nouvelle date :")
     #ou msg = "Date incorrecte"
+    #puis print(msg)
 
 #Pour une année bisextile :
 if mois == 2 :
-    if ((année ))
+    if (annee%4 == 0 and annee%100!=0) or (annee%100 == 0) :
 
 
-print (msg)
