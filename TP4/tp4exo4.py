9170244f19b10cbf9e8f3f96e0339b99ba198cc4
e2400096 <chloesondag.pro@gmail.com>
2024-12-11 09:55:24 +0100

    yo

--- TP4/tp4exo4.py
+++ TP4/tp4exo4.py
@@ -1,10 +1,28 @@
-entiers=[]
+L1 = [2, 7, 5, 6, 7, 1, 6, 2, 1, 7, 6]
+
+""" ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** *
+* Completez le programme a partir d'ici.
+** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** * /
+"""
+
+
 
 
 occurrence=0
 
 
 print(f"Le nombre le plus frequent dans la liste est le : , {nombre} ({occurence} x)")
+
+
+
+
+
+
+""" ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** *
+* Ne rien modifier apres cette ligne.
+** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** * /
+"""
+
 
 #exemple:
 #print(f"Le nombre le plus frequent dans la liste est le : , {} ({} x)")
