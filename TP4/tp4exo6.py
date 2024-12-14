5c72357358d1382d86dcb3b253917c89c574aef2
chlo070 <chloesondag.pro@gmail.com>
2024-12-14 07:20:00 +0100

    /

--- TP4/tp4exo6.py
+++ TP4/tp4exo6.py
@@ -1,7 +1,19 @@
+tab=[5,2,4,8,1,3]
 
 
+#Partie 1 avec print(sorted(tab))
+for i in range (len(tab)) :
+    for j in range (len(tab)-1):
+        if j < i :
+            temp = tab[i]
+            tab[i]=tab[j]
+            tab[j]=temp
 
-# d'abord avec
+
 print(sorted(tab))
-#puis avec
-tab.sort()
+
+
+
+
+
+#Partie 2 avec tab.sort()
