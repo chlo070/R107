710b8d48132550979e8bcbc1c8391372f57592a2
chlo070 <chloesondag.pro@gmail.com>
2024-12-14 09:09:12 +0100

    yo

--- TP4/tp4exo8_dictionnaires.py
+++ TP4/tp4exo8_dictionnaires.py
@@ -14,13 +14,21 @@ dico['firstname'] = 'Chloé'
 dico['name'] = 'Sondag'
 dico['promo'] = 'RT1 2024'
 dico['group'] = 'RT132'
-#print(dico)
-print(f"Votre nom est {'name'}, prénom est {'firstname'}, vous faites partie de la promo {'promo'} et votre groupe est {'group'}")
+print(dico)
+#print(f"Votre nom est {'name'}, prénom est {'firstname'}, vous faites partie de la promo {'promo'} et votre groupe est {'group'}")
+print(f"Les clés du dictionnaire sont :,")
+for i in range (len(dico)) :
+    print(f"-dico.keys(i)")
+print(f"Les valeurs du dictionnaire sont :,")
+for i in range (len(dico)) :
+    print(f"-dico.values(i)")
+print(f"Les tuplets du dictionnaire sont :,")
+for i in range (len(dico),
+    print(f"-dico.items(i)")
 
 #Affichage avec les méthodes keys(), values() et items()
 """
-. Un exemple d’affichage à produire pour le dictionnaire 
-dic={"name":"toto","firstname":"titi","promo":2022,"group":202} :
+Un exemple d’affichage à produire pour le dictionnaire : dic={"name":"toto","firstname":"titi","promo":2022,"group":202} :
  
 Les clés du dictionnaire sont :
 -name
