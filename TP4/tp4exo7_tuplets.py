a6f56487f00ad8bef1eb5a54f4fd7e0ba2ea2093
chlo070 <chloesondag.pro@gmail.com>
2024-12-14 10:52:18 +0100

    yo

--- TP4/tp4exo7_tuplets.py
+++ TP4/tp4exo7_tuplets.py
@@ -1,10 +1,15 @@
 #tuplet binome
 
 binome=('chloe.sondag', 'lea yang')
-print(f"Le binôme actuel est ", {binome})
+#print(f"Le binôme actuel est ", {binome})
 
-str="L’étudiant chloe.sondag est en binome avec l’étudiant lea.yang"
+phrase=str("L’étudiant(e) chloe.sondag est en binome avec l’étudiant(e) lea.yang")
+print(phrase)
 
-print(f"Indiquez le nouveau binôme de", {binome[0]},": ")
-nvo=str(input(binome[1]))
-"""
+nvo=str(input(f"Indiquez le nouveau binôme de {binome[0]} : "))
+
+print(binome)
+del(binome[1])
+print(binome)
+#trinome = append.binome('lili yang')
+
