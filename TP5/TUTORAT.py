9d453de7f1c7ff61147b1f21d92c7c09060c7952
chlo070 <chloesondag.pro@gmail.com>
2024-12-17 13:10:11 +0100

    tutorat

---
+++ TP5/TUTORAT.py
@@ -0,0 +1,24 @@
+somme = 627
+
+nbCent = 0
+nbCinquante = 0
+nbVingt = 0
+nbDeux = 0
+nbUn = 0
+
+while somme >=100
+    somme -= 100
+    nbCent += 1
+while somme >=50
+    somme -= 50
+    nbCinquante += 1
+while somme >=10
+    somme -= 10
+    nbDix += 1
+while somme >=2
+    somme -= 2
+    nbDeux += 1
+while somme >=1
+    somme -= 1
+    nbUn += 1
+
